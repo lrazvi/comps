d29873f8fde3eb52585f637ca78b110afa9f123e
lrazvi <lrazvi@oxy.edu>
2022-11-11 14:56:31 -0800

    task lst edits and nav

---
+++ src/components/calendar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Dogs = () =>{
+    return (
+      <div>
+        <h3>Dogs</h3>
+        <div>
+          <h1>Meow</h1>
+        </div>
+      </div>
+    );
+  }
+  export default Dogs;
+  
+  //calendar.js
--- src/tasks.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import  ReactDOM  from "react-dom/client";
-import './style.css';
-
-export default function Tasks(){
-    return <h1>Takss</h1>
-}
